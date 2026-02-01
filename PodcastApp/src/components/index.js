0230cd29e039b1976ad9fb3c1bca86420059911e
David Aniekan <203154494+Davidaniekan@users.noreply.github.com>
2026-02-01 22:37:18 +0200

    refactor: centralize component exports in index

---
+++ PodcastApp/src/components/index.js
@@ -0,0 +1,11 @@
+// UI Components
+export { Header, Loading, Pagination, GenreTags } from "./UI";
+
+// Podcast Components
+export { PodcastCard, PodcastGrid, PodcastDetail } from "./Podcasts";
+
+// Layout Components
+export { Container } from "./Layout";
+
+// Filter Components
+export { GenreFilter, SearchBar, SortSelect } from "./Filters";
